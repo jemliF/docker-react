241dd3d76e087ee6e3ab3b144b1b5f0f067d4acf
jemliF <jemlifathi2013@gmail.com>
2020-06-05 17:47:29 +0200

    changed app text: fix tests

--- src/App.js
+++ src/App.js
@@ -8,8 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed on feature branch
+        I was changed on feature branch
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
